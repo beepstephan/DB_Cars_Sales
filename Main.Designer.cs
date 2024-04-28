5a4587d92e25a74c0a5682fa57845f65fee92a1c
SmokinSexyStas <stas.zp.619@gmail.com>
2024-04-28 22:07:15 +0300

    interface + dealership form

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -28,34 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.testDataGridView = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.testDataGridView)).BeginInit();
-            this.SuspendLayout();
+            tabControl1 = new TabControl();
+            tabPageCars = new TabPage();
+            tabPageCarModels = new TabPage();
+            tabPageCarDealerships = new TabPage();
+            buttonAddDealership = new Button();
+            testDataGridView = new DataGridView();
+            tabPageEmployees = new TabPage();
+            tabPageCustomers = new TabPage();
+            tabPageTransactions = new TabPage();
+            tabControl1.SuspendLayout();
+            tabPageCarDealerships.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)testDataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPageCars);
+            tabControl1.Controls.Add(tabPageCarModels);
+            tabControl1.Controls.Add(tabPageCarDealerships);
+            tabControl1.Controls.Add(tabPageEmployees);
+            tabControl1.Controls.Add(tabPageCustomers);
+            tabControl1.Controls.Add(tabPageTransactions);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Multiline = true;
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1054, 596);
+            tabControl1.TabIndex = 1;
+            // 
+            // tabPageCars
+            // 
+            tabPageCars.Location = new Point(4, 24);
+            tabPageCars.Name = "tabPageCars";
+            tabPageCars.Padding = new Padding(3);
+            tabPageCars.Size = new Size(1046, 568);
+            tabPageCars.TabIndex = 0;
+            tabPageCars.Text = "Cars";
+            tabPageCars.UseVisualStyleBackColor = true;
+            // 
+            // tabPageCarModels
+            // 
+            tabPageCarModels.Location = new Point(4, 24);
+            tabPageCarModels.Name = "tabPageCarModels";
+            tabPageCarModels.Padding = new Padding(3);
+            tabPageCarModels.Size = new Size(1046, 568);
+            tabPageCarModels.TabIndex = 1;
+            tabPageCarModels.Text = "Car Models";
+            tabPageCarModels.UseVisualStyleBackColor = true;
+            // 
+            // tabPageCarDealerships
+            // 
+            tabPageCarDealerships.Controls.Add(buttonAddDealership);
+            tabPageCarDealerships.Controls.Add(testDataGridView);
+            tabPageCarDealerships.Location = new Point(4, 24);
+            tabPageCarDealerships.Name = "tabPageCarDealerships";
+            tabPageCarDealerships.Padding = new Padding(3);
+            tabPageCarDealerships.Size = new Size(1046, 568);
+            tabPageCarDealerships.TabIndex = 2;
+            tabPageCarDealerships.Text = "Car Dealerships";
+            tabPageCarDealerships.UseVisualStyleBackColor = true;
+            // 
+            // buttonAddDealership
+            // 
+            buttonAddDealership.Location = new Point(8, 15);
+            buttonAddDealership.Name = "buttonAddDealership";
+            buttonAddDealership.Size = new Size(98, 44);
+            buttonAddDealership.TabIndex = 2;
+            buttonAddDealership.Text = "Додати автосалон";
+            buttonAddDealership.UseVisualStyleBackColor = true;
+            buttonAddDealership.Click += buttonAddDealership_Click;
             // 
             // testDataGridView
             // 
-            this.testDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.testDataGridView.Location = new System.Drawing.Point(12, 12);
-            this.testDataGridView.Name = "testDataGridView";
-            this.testDataGridView.RowTemplate.Height = 25;
-            this.testDataGridView.Size = new System.Drawing.Size(1027, 624);
-            this.testDataGridView.TabIndex = 0;
+            testDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            testDataGridView.Location = new Point(112, 0);
+            testDataGridView.Name = "testDataGridView";
+            testDataGridView.RowTemplate.Height = 25;
+            testDataGridView.Size = new Size(934, 568);
+            testDataGridView.TabIndex = 1;
+            // 
+            // tabPageEmployees
+            // 
+            tabPageEmployees.Location = new Point(4, 24);
+            tabPageEmployees.Name = "tabPageEmployees";
+            tabPageEmployees.Size = new Size(1046, 568);
+            tabPageEmployees.TabIndex = 3;
+            tabPageEmployees.Text = "Employees";
+            tabPageEmployees.UseVisualStyleBackColor = true;
+            // 
+            // tabPageCustomers
+            // 
+            tabPageCustomers.Location = new Point(4, 24);
+            tabPageCustomers.Name = "tabPageCustomers";
+            tabPageCustomers.Size = new Size(1046, 568);
+            tabPageCustomers.TabIndex = 4;
+            tabPageCustomers.Text = "Customers";
+            tabPageCustomers.UseVisualStyleBackColor = true;
+            // 
+            // tabPageTransactions
+            // 
+            tabPageTransactions.Location = new Point(4, 24);
+            tabPageTransactions.Name = "tabPageTransactions";
+            tabPageTransactions.Size = new Size(1046, 568);
+            tabPageTransactions.TabIndex = 5;
+            tabPageTransactions.Text = "Transactions";
+            tabPageTransactions.UseVisualStyleBackColor = true;
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1051, 648);
-            this.Controls.Add(this.testDataGridView);
-            this.Name = "Main";
-            this.Text = "Main";
-            ((System.ComponentModel.ISupportInitialize)(this.testDataGridView)).EndInit();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1054, 596);
+            Controls.Add(tabControl1);
+            Name = "Main";
+            Text = "Main";
+            tabControl1.ResumeLayout(false);
+            tabPageCarDealerships.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)testDataGridView).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
-
+        private TabControl tabControl1;
+        private TabPage tabPageCars;
+        private TabPage tabPageCarModels;
+        private TabPage tabPageCarDealerships;
+        private TabPage tabPageEmployees;
+        private TabPage tabPageCustomers;
+        private TabPage tabPageTransactions;
         private DataGridView testDataGridView;
+        private Button buttonAddDealership;
     }
 }
--- Main.cs
+++ Main.cs
@@ -44,5 +44,10 @@ namespace DB_Cars_Sales
             MessageBox.Show("кулитии!");
         }
 
+        private void buttonAddDealership_Click(object sender, EventArgs e)
+        {
+            FormAddDealership formAddDealership = new FormAddDealership();
+            formAddDealership.ShowDialog();
+        }
     }
 }
